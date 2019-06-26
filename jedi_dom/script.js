5cc12f6c738d530321b6c59a784e1049923c099e
Marc Wright <marc.wright@gmail.com>
2019-06-26 10:19:34 +0300

    Fix jquery link and add script.js

---
+++ jedi_dom/script.js
@@ -0,0 +1,26 @@
+// 1. Make the Luke and Obiwan lightsabers appear in the browser
+
+
+
+
+// 2. Change the text on the first 'p' tag to "Changing text!!"
+
+
+
+
+// 3. Change the color of the 'h1'
+
+
+
+
+// 4. Change the length and/or color of a lightsaber
+
+
+
+
+// 5. Change the font
+
+
+
+
+// 6. Make Vader's handle blue.
